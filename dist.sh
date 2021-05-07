e076173bf2aa4780dd84467cdd32620a3f09fa7d
gcxfd <i@rmw.link>
2021-05-07 16:56:44 +0800

    .

--- dist.sh
+++ dist.sh
@@ -8,7 +8,7 @@ version="./version.txt"
 next=$(cat $version | awk -F. -v OFS=. '{$NF++;print}')
 echo $next > $version
 echo $next
-echo "export default '$version'" > src/version.coffee
+echo "export default '$next'" > src/version.coffee
 
 
 sed -i "s/@[0-9]\+.[0-9]\+.[0-9]\+\//@$next\//g" readme.make.md
